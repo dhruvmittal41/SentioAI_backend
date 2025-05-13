f751ff47fce4d0a7f024248aea2dd0e9cc4a2a1a
Dhruv <mittaldhruv41@gmail.com>
2025-05-13 20:18:40 +0530

    fixes

--- auth/deployment_settings.py
+++ auth/deployment_settings.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 MIDDLEWARE = [
@@ -40,7 +40,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get["DATABASE_URL"],
+        default=os.environ["DATABASE_URL"],
         conn_max_age=600,
     )
 }
